1c7d6e49c40140d2cdeeb017cc3bba4ec93e3d39
aKuad <53811809+aKuad@users.noreply.github.com>
2023-06-10 00:22:22 +0900

    Src upload - python

---
+++ python/MyClass.py
@@ -0,0 +1,38 @@
+"""Source file description
+
+Source file detail description
+
+"""
+
+"""Class description
+
+Class detail description
+
+Attributes:
+  member (int): Member description
+
+"""
+class Class:
+  member: int = 2
+
+  def func(arg: int):
+    """Method description
+
+    Method detail description
+
+    Example:
+      Examples here::
+
+        ret = func(3)
+
+    Note:
+      Note of method
+
+    Args:
+      arg(int): Argument description
+    
+    Return:
+      Return description
+
+    """
+    return arg
---
+++ python/func.py
@@ -0,0 +1,27 @@
+"""Source file description
+
+Source file detail description
+
+"""
+
+def func(arg: int):
+  """Function description
+
+  Function detail description
+
+  Example:
+    Examples here::
+
+      ret = func(3)
+
+  Note:
+    Note of function
+
+  Args:
+    arg(int): Argument description
+  
+  Return:
+    Return description
+
+  """
+  return arg
