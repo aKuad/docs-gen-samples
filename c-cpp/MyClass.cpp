0d4d540750911909b48d80b56588d36267a69726
aKuad <53811809+aKuad@users.noreply.github.com>
2023-06-09 23:27:59 +0900

    Src upload - c/c++

---
+++ c-cpp/MyClass.cpp
@@ -0,0 +1,51 @@
+/**
+ * @file class.cpp
+ *
+ * @brief Source file description
+ *
+ * @details
+ * Source file detail description
+ *
+ * @author aKuad
+ */
+
+
+/**
+ * @brief AnyClass description
+ *
+ * @details
+ * AnyClass detail description
+ */
+class MyClass {
+  private:
+  /**
+   * @brief Member description
+   *
+   * @details
+   * Member detail description
+   */
+  static int member;
+
+  public:
+  /**
+   * @brief Function description
+   *
+   * @details
+   * Function detail description
+   *
+   * Example:
+   *
+   * @code
+   * MyClass *my_class = new MyClass();
+   * int res = my_class->func(3);
+   * // 3
+   * @endcode
+   *
+   * @note
+   * Note of function
+   *
+   * @param[in] arg Argument description
+   * @return Return description
+   */
+  int func(int arg) { return arg; }
+};
---
+++ c-cpp/func.c
@@ -0,0 +1,33 @@
+/**
+ * @file func.c
+ *
+ * @brief Source file description
+ *
+ * @details
+ * Source file detail description
+ *
+ * @author aKuad
+ */
+
+/**
+ * @brief Function description
+ *
+ * @details
+ * Function detail description
+ * 
+ * Example:
+ *
+ * @code
+ * int res = func(3);
+ * // 3
+ * @endcode
+ *
+ * @note
+ * Note of function
+ *
+ * @param[in] arg Argument description
+ * @return Return description
+ */
+int func(int arg) {
+  return arg;
+}
