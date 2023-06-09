e6ac3475193bd8827be7c7076278ef5815af62aa
aKuad <53811809+aKuad@users.noreply.github.com>
2023-06-09 22:32:26 +0900

    Src upload - js

---
+++ javascript/MyClass.js
@@ -0,0 +1,33 @@
+/**
+ * @summary Class description
+ *
+ * @description
+ * Class detail description
+ */
+class MyClass {
+  /**
+   * @summary Member description
+   *
+   * @description
+   * Member detail description
+   */
+  #member = 2;
+
+  /**
+   * @summary Function description
+   *
+   * @description
+   * Function detail description
+   *
+   * @example
+   * obj = new MyClass();
+   * res = obj.func(3);
+   * // 3
+   *
+   * @param {number} arg Argument description
+   * @returns Return description
+   */
+  func(arg) {
+   return arg;
+  }
+}
---
+++ javascript/func.js
@@ -0,0 +1,25 @@
+/**
+ * @file Source file description
+ * 
+ * @description
+ * Source file detail description
+ *
+ * @author aKuad
+ */
+
+/**
+ * @summary Function description
+ *
+ * @description
+ * Function detail description
+ *
+ * @example
+ * res = func(3);
+ * // 3
+ *
+ * @param {number} arg Argument description
+ * @returns Return description
+ */
+function func(arg) {
+  return arg;
+}
